a7bfce53a1a0c672762e431dba506c436bebabc2
T1\reda.aaslaoui <reda.aaslaoui@traceone.com>
2019-10-23 16:26:13 +0200

    Add Elo algorithm for calculation of score

---
+++ FacemashService/IEloCalculation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacemashModel.Enums;
+
+namespace FacemashService
+{
+    public interface IEloCalculation
+    {
+        void CalculateELO(int playerOneRating, int playerTwoRating, GameOutcome outcome);
+    }
+}
